c2a635e5cf8df2f046d40a3251e13c570721eea5
LakshmiDeepak27 <lakshmideepak.mpc1836@gmail.com>
2026-09-26 23:18:07 +0530

    fix: define __dirname in server.js ESM scope

--- server/src/server.js
+++ server/src/server.js
@@ -17,6 +17,7 @@ import fs from 'fs';
 import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const candidateDistPaths = [
   path.resolve(__dirname, '../../client/dist'),
   path.resolve(process.cwd(), 'client/dist'),
